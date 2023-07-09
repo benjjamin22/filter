d272ca2a88e0473b1ad6bcba952c0d0a8f27dcb1
benjamin <deeprising4ilife@gmail.com>
2023-07-08 16:02:15 -0800

    Updates

--- NUASA(IMSU)/script.js
+++ NUASA(IMSU)/script.js
@@ -1345,3 +1345,8 @@ function getmovie() {
 
     });
 }
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+let vw = window.innerWidth * 0.01;
+document.documentElement.style.setProperty('--vw', `${vw}px`);
