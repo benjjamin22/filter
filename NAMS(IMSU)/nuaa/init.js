c4eca2ebb25aa629950ab6fdeafef1b8e7b25b67
benjamin <deeprising4ilife@gmail.com>
2023-12-22 19:14:08 -0800

    Updates

--- NAMS(IMSU)/nuaa/init.js
+++ NAMS(IMSU)/nuaa/init.js
@@ -3,3 +3,34 @@ const auth = new Auth();
 document.querySelector(".logout").addEventListener("click", (e) => {
     auth.logOut();
 });
+
+
+var warningTimeoutID = undefined;
+var logoutTimeoutID = undefined;
+const events = ["click", "mousemove", "mousedown", "keydown"];
+const body = document.querySelector("body");
+
+window.addEventListener("DOMContentLoaded", () => {
+    if (window.location.pathname === "/nuaa/sample.html") {
+        warningTimeoutID = setTimeout(callTimeoutFunc, 5000);
+        events.forEach((event) => {
+            window.addEventListener(event, eventHandler);
+        });
+    }
+});
+
+function callTimeoutFunc() {
+    logoutTimeoutID = setTimeout(() => {
+        auth.logOut();
+    }, 4000);
+}
+
+function eventHandler() {
+    if (logoutTimeoutID) {
+        clearTimeout(logoutTimeoutID);
+        if (body.children[0].classList.contains("warning"))
+            body.removeChild(body.firstElementChild);
+    }
+    clearTimeout(warningTimeoutID);
+    warningTimeoutID = setTimeout(callTimeoutFunc, 5000);
+}
