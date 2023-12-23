7fcdb0296e27d52101b9d8e37c13b0af09da4fcb
benjamin <deeprising4ilife@gmail.com>
2023-12-23 08:57:05 -0800

    Updates

--- NAMS(IMSU)/nuaa/init.js
+++ NAMS(IMSU)/nuaa/init.js
@@ -4,34 +4,33 @@ document.querySelector(".logout").addEventListener("click", (e) => {
     auth.logOut();
 });
 
+// var warningTimeoutID = undefined;
+// var logoutTimeoutID = undefined;
+// const events = ["click", "mousemove", "mousedown", "keydown"];
+// const body = document.querySelector("body");
 
-var warningTimeoutID = undefined;
-var logoutTimeoutID = undefined;
-const events = ["click", "mousemove", "mousedown", "keydown"];
-const body = document.querySelector("body");
+// window.addEventListener("DOMContentLoaded", () => {
+////  if (window.location.href.indexOf("sample.html") != -1) {
+////////warningTimeoutID = setTimeout(callTimeoutFunc, 5000);
+////////events.forEach((event) => {
+////////// window.addEventListener(event, eventHandler);
+////////});
+////  }
+// });
 
-window.addEventListener("DOMContentLoaded", () => {
-    if (window.location.href.indexOf("sample.html") != -1) {
-        warningTimeoutID = setTimeout(callTimeoutFunc, 5000);
-        events.forEach((event) => {
-            window.addEventListener(event, eventHandler);
-        });
-    }
-});
+// function callTimeoutFunc() {
+////  logoutTimeoutID = setTimeout(() => {
+////////auth.logOut();
+////////window.location.replace("NAMS(IMSU)/index.html");
+////  }, 4000);
+// }
 
-function callTimeoutFunc() {
-    logoutTimeoutID = setTimeout(() => {
-        auth.logOut();
-        window.location.replace("NAMS(IMSU)/index.html");
-    }, 4000);
-}
-
-function eventHandler() {
-    if (logoutTimeoutID) {
-        clearTimeout(logoutTimeoutID);
-        if (body.children[0].classList.contains("warning"))
-            body.removeChild(body.firstElementChild);
-    }
-    clearTimeout(warningTimeoutID);
-    warningTimeoutID = setTimeout(callTimeoutFunc, 5000);
-}
+// function eventHandler() {
+////  if (logoutTimeoutID) {
+////////clearTimeout(logoutTimeoutID);
+////////if (body.children[0].classList.contains("warning"))
+////////// body.removeChild(body.firstElementChild);
+////  }
+////  clearTimeout(warningTimeoutID);
+////  warningTimeoutID = setTimeout(callTimeoutFunc, 5000);
+// }
