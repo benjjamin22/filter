470c5d195daae24ace7e76b7a9492eade590d33d
benjamin <deeprising4ilife@gmail.com>
2023-12-23 07:37:47 -0800

    Updates

--- NAMS(IMSU)/nuaa/init.js
+++ NAMS(IMSU)/nuaa/init.js
@@ -11,7 +11,7 @@ const events = ["click", "mousemove", "mousedown", "keydown"];
 const body = document.querySelector("body");
 
 window.addEventListener("DOMContentLoaded", () => {
-    if (window.location.pathname === "https://mydatabase.com.ng/NAMS(IMSU)/nuaa/sample.html") {
+    if (window.location = "sample.html") {
         warningTimeoutID = setTimeout(callTimeoutFunc, 5000);
         events.forEach((event) => {
             window.addEventListener(event, eventHandler);
