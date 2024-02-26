9fccd56c15ac28a196b9e37c3b73ae3166ce5fe0
benjamin <deeprising4ilife@gmail.com>
2024-02-25 17:01:44 -0800

    Updates

---
+++ re/assets/js/main.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //------ Slider Begin
+    const CaroS = document.querySelector('.swiper-wrapper');
+    const CaroSlider = new MicroSlider(CaroS, { indicators: true, indicatorText: '' });
+    const hammer = new Hammer(CaroS);
+    const CaroSTimer = 2000;
+    let CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+
+});
+
+/*=============== SWIPER JS ===============*/
+
+let swiperCards = new Swiper(".card__content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+        968: {
+            slidesPerView: 3,
+        },
+    },
+});
